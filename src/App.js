7901bf2f20f0a36aaacd60ce3671838c6a50fd75
Sudarsan Kumar <sudarsanmitc@gmail.com>
2021-08-20 10:14:11 +0530

    Added source Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Facebook from './components/Facebook'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Facebook auth example</h1>
+      <p>To get started authentiacte with facebook</p>
+      <Facebook />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Facebook.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import FacebookLogin from 'react-facebook-login'
+export default class Facebook extends Component {
+    state = {
+        isLoggedin : false,
+        userID : '',
+        name : '',
+        email : '',
+        picture : ''
+    }
+    responseFacebook = response => {
+        this.setState({
+            isLoggedin:true,
+            userID:response.userID,
+            name:response.name,
+            email:response.email,
+            picture:response.picture.data.url
+        })
+    }
+    componentClicked = () => {
+        console.log('clicked')
+    }
+    render() {
+        let fbContent
+        if(this.state.isLoggedin)
+        {
+            fbContent = (
+                <div style={{
+                    width:'400px',
+                    margin:'auto',
+                    background:'#f4f4f4',
+                    padding:'20px'
+                }}>
+                    <img src={this.state.picture} alt={this.state.name}></img>
+                    <h2>Welcome {this.state.name}</h2>
+                    Email : {this.state.email}
+                </div>
+            )
+        }
+        else
+        {
+            fbContent = (
+                <FacebookLogin
+                    appId="1443679602643305"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    onClick={this.componentClicked}
+                    callback={this.responseFacebook} 
+                />
+            )
+        }
+        return (
+            <div>
+                {fbContent}
+            </div>
+        )
+    }
+}
